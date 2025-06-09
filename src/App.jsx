3a9aa67b509b3d194b1a085c6f9fe1fd92f959e2
Malik Elmessiry <malik.messiry@gmail.com>
2025-06-09 12:45:12 +0200

    set up props

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,26 @@ import StudentList from './components/StudentList';
 import ClassInfo from './components/ClassInfo';
 
 function App() {
+  const studentData = [
+    {
+      name: 'Ada',
+      email: 'ada@dev.org'
+    },
+    {
+      name: 'Soo-ah',
+      email: 'sooah@dev.org'
+    },
+    {
+      name: 'Chrissy',
+      email: 'chrissy@dev.org'
+    }
+  ];
 
   return (
     <main>
       <h1>Attendance</h1>
-      <ClassInfo></ClassInfo>
-      <StudentList></StudentList>
+      <ClassInfo memberCount={studentData.length}></ClassInfo>
+      <StudentList students={studentData}></StudentList>
     </main>
   );
 }
